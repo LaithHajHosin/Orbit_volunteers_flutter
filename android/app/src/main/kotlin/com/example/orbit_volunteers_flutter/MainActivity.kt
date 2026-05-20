66a32e06c78dad81d67ac493e3c6dc3b1f14105d
Admin <sanaakhalf006@gmail.com>
2026-05-20 16:56:24 +0300

    initial project. add empty home screen.

---
+++ android/app/src/main/kotlin/com/example/orbit_volunteers_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.orbit_volunteers_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:orbit_volunteers_flutter/Home/Home_Screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.routeName,
+      routes: {HomeScreen.routeName: (context) => HomeScreen()},
+    );
+  }
+}
